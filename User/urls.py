2c790d5cd8ea724fa4cd7a6de2e7553b382cbdb9
Aldrin Saurov Sarker <A.saurov2016@gmail.com>
2022-09-08 22:07:02 +0600

    commit

--- User/urls.py
+++ User/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('covid-statement', views.covidStatement, name='covid'),
-    path('search', views.search, name='search'),
-    path('autosuggest', views.autosuggest, name='autosuggest'),
-    path('about-us', views.aboutUs, name='aboutUs'),
-    path('contact-us', views.contactUs, name='contactUs'),
-    path('request-call-back', views.requestCallback, name='requestCallback'),
-    path('services/<id>', views.service, name='service'),
+    path('covid-statement/', views.covidStatement, name='covid'),
+    path('search/', views.search, name='search'),
+    path('autosuggest/', views.autosuggest, name='autosuggest'),
+    path('about-us/', views.aboutUs, name='aboutUs'),
+    path('contact-us/', views.contactUs, name='contactUs'),
+    path('request-call-back/', views.requestCallback, name='requestCallback'),
+    path('services/<id>/', views.service, name='service'),
 ]
