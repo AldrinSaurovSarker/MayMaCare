6ed5cfdd9a45bc8b444af2351fff8d040ff9301c
Aldrin Saurov Sarker <A.saurov2016@gmail.com>
2022-09-08 21:21:02 +0600

    commit

--- MayMaCare/urls.py
+++ MayMaCare/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import static
 
 
 urlpatterns = [
